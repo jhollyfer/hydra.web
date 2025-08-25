5070fd302aea5762b4bc8001e631a6bc78142bc4
Jhollyfer <jhollyfer.fr@gmail.com>
2025-08-25 11:53:20 -0500

    head

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,12 +2,9 @@ import { createRootRoute, HeadContent, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: () => (
-    <main
-      // className="relative flex flex-col h-screen w-full overflow-hidden"
-      className="relative flex flex-col h-screen w-full"
-    >
+    <>
       <HeadContent />
       <Outlet />
-    </main>
+    </>
   ),
 });
--- src/routes/_landing-page/index.tsx
+++ src/routes/_landing-page/index.tsx
@@ -10,13 +10,14 @@ export const Route = createFileRoute("/_landing-page/")({
   head: () => ({
     meta: [
       // Meta tags básicas,
-      {
-        title: "Mangangá - Site Oficial do Boi Bumbá",
-      },
+
       {
         name: "description",
         content:
           "Site oficial do Boi Bumbá Mangangá - Tradição, cultura e festa popular brasileira",
+      },
+      {
+        title: "Mangangá - Site Oficial do Boi Bumbá",
       },
       {
         name: "robots",
@@ -29,48 +30,48 @@ export const Route = createFileRoute("/_landing-page/")({
 
       // Open Graph Protocol
       {
-        property: "og:type",
+        name: "og:type",
         content: "website",
       },
       {
-        property: "og:site_name",
+        name: "og:site_name",
         content: "Mangangá",
       },
       {
-        property: "og:title",
+        name: "og:title",
         content: "Mangangá - Site Oficial do Boi Bumbá",
       },
       {
-        property: "og:description",
+        name: "og:description",
         content:
           "Site oficial do Boi Bumbá Mangangá - Tradição, cultura e festa popular brasileira",
       },
       {
-        property: "og:url",
+        name: "og:url",
         content: "https://www.manganga.maiyu.com.br",
       },
       {
-        property: "og:image",
+        name: "og:image",
         content: "https://www.manganga.maiyu.com.br/og-image.jpg",
       },
       {
-        property: "og:image:type",
+        name: "og:image:type",
         content: "image/jpeg",
       },
       {
-        property: "og:image:width",
+        name: "og:image:width",
         content: "1200",
       },
       {
-        property: "og:image:height",
+        name: "og:image:height",
         content: "630",
       },
       {
-        property: "og:image:alt",
+        name: "og:image:alt",
         content: "Logotipo do Boi Bumbá Mangangá",
       },
       {
-        property: "og:locale",
+        name: "og:locale",
         content: "pt_BR",
       },
 
